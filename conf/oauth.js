f47c0e4ac8157b9b76c83671233253e357d73fd0
Luis Malaga <malaga@starbrand.mx>
2018-08-24 16:41:27 -0500

    24 agosto 2018 4:41pm

--- conf/oauth.js
+++ conf/oauth.js
@@ -2,17 +2,17 @@ var config = {
 	facebook:{
 		clientID: '870496999775638',
 		clientSecret: '890cc3007255067044a4a862a6fcedfc',
-		callbackURL: 'http://127.0.0.1:5000/login/facebook/callback'
+		callbackURL: 'https://glowrun5k.com.mx/login/facebook/callback'
 	},
 	google:{
 		clientID: "65502020027-2hcilnkgsbeu4n0g1agt8fd2uicgkf0f.apps.googleusercontent.com",
 		clientSecret: "ucfMnrabQOlmwwZp3gTFM4o8",
-		callbackURL: "http://127.0.0.1:5000/login/google/callback"
+		callbackURL: "https://glowrun5k.com.mx/login/google/callback"
 	},
 	twitter:{
 		consumerKey: "OabNHqICoIV9WAviIb0gkpiom",
 		consumerSecret: "4IFGuDpxM2QxxGvUlLafr8gnfES4bKSSE1tTWUS3xhXKQm4c28",
-		callbackURL: "http://127.0.0.1:5000/login/twitter/callback"
+		callbackURL: "https://glowrun5k.com.mx/login/twitter/callback"
 	},
 	instagram:{
 		clientID: "54a3c81dabf94f89ae1e473e707f28d6",
